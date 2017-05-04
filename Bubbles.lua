4611e39bdffb08d65cef607407b5ae2c1349b8fb
Benjamin Staneck <staneck@gmail.com>
2017-05-04 18:48:05 +0200

    whitespace

--- Bubbles.lua
+++ Bubbles.lua
@@ -35,7 +35,7 @@ L:AddLocale("enUS", {
 	font_name = "Use Chat Font",
 	font_desc = "Use the same font you are using on the chatframe",
 	fontsize_name = "Font Size",
-	fontsize_desc = "Set the chat bubble font size",	
+	fontsize_desc = "Set the chat bubble font size",
 })
 --@end-debug@
 
@@ -44,31 +44,31 @@ L:AddLocale("enUS", {
 
 
 --[===[@non-debug@
-L:AddLocale("enUS", 
+L:AddLocale("enUS",
 --@localization(locale="enUS", format="lua_table", same-key-is-true=true)@
 )
-L:AddLocale("frFR",  
+L:AddLocale("frFR",
 --@localization(locale="frFR", format="lua_table", same-key-is-true=true)@
 )
-L:AddLocale("deDE", 
+L:AddLocale("deDE",
 --@localization(locale="deDE", format="lua_table", same-key-is-true=true)@
 )
-L:AddLocale("koKR",  
+L:AddLocale("koKR",
 --@localization(locale="koKR", format="lua_table", same-key-is-true=true)@
 )
-L:AddLocale("esMX",  
+L:AddLocale("esMX",
 --@localization(locale="esMX", format="lua_table", same-key-is-true=true)@
 )
-L:AddLocale("ruRU",  
+L:AddLocale("ruRU",
 --@localization(locale="ruRU", format="lua_table", same-key-is-true=true)@
 )
-L:AddLocale("zhCN",  
+L:AddLocale("zhCN",
 --@localization(locale="zhCN", format="lua_table", same-key-is-true=true)@
 )
-L:AddLocale("esES",  
+L:AddLocale("esES",
 --@localization(locale="esES", format="lua_table", same-key-is-true=true)@
 )
-L:AddLocale("zhTW",  
+L:AddLocale("zhTW",
 --@localization(locale="zhTW", format="lua_table", same-key-is-true=true)@
 )
 --@end-non-debug@]===]
@@ -84,20 +84,20 @@ local defaults = {
         font = true,
         fontsize = 14,
 	}
-} 
+}
 
 
 
 local toggleOption = {
 		name = function(info) return L[info[#info].."_name"] end,
 		desc = function(info) return L[info[#info].."_desc"] end,
-		type="toggle", 
+		type="toggle",
 }
 
 local options =  {
     handler = addon,
     get = "GetValue",
-    set = "SetValue",	
+    set = "SetValue",
 
     name = L["module_name"],
     desc = L["module_desc"],
@@ -110,7 +110,7 @@ local options =  {
         fontsize = {
         	name = L.fontsize_name,
         	desc = L.fontsize_desc,
-        	type="range", min=8, max=32, step=1, order=101            
+        	type="range", min=8, max=32, step=1, order=101
         }
 	}
 }
@@ -125,17 +125,17 @@ local BUBBLE_SCAN_THROTTLE = 0.1
 
 function addon:OnInitialize()
     self.db = LibStub("AceDB-3.0"):New("BubbliciousDB", defaults, "Default")
-    
+
 	local acreg = LibStub("AceConfigRegistry-3.0")
 	acreg:RegisterOptionsTable(L.module_name, options)
 
 	local acdia = LibStub("AceConfigDialog-3.0")
 	acdia:AddToBlizOptions(L.module_name, L.module_name)
 
-    
+
     local cmd_name = L.module_name:upper()
-	SlashCmdList[cmd_name] = 
-    	function()     
+	SlashCmdList[cmd_name] =
+    	function()
         	local acd = LibStub("AceConfigDialog-3.0")
             if acd.OpenFrames[L.module_name] then
                 acd:Close(L.module_name)
@@ -143,7 +143,7 @@ function addon:OnInitialize()
                 acd:Open(L.module_name)
             end
         end
-        
+
     _G["SLASH_"..cmd_name.."1"] = "/"..cmd_name:lower()
 end
 
@@ -152,8 +152,8 @@ function addon:OnEnable()
     self.update = self.update or CreateFrame('Frame');
     self.throttle = BUBBLE_SCAN_THROTTLE
 
-    self.update:SetScript("OnUpdate", 
-        function(frame, elapsed) 
+    self.update:SetScript("OnUpdate",
+        function(frame, elapsed)
             self.throttle = self.throttle - elapsed
             if frame:IsShown() and self.throttle < 0 then
                 self.throttle = BUBBLE_SCAN_THROTTLE
@@ -180,9 +180,9 @@ function addon:ApplyOptions()
 	self.shorten = self.db.profile.shorten
 	self.color = self.db.profile.color
 	self.icons = self.db.profile.icons
-    self.font = self.db.profile.font 
+    self.font = self.db.profile.font
     self.fontsize = self.db.profile.fontsize
-	
+
 	if self.shorten or self.color or self.format or self.icons or self.font then
 	    self.update:Show()
 	else
@@ -196,7 +196,7 @@ end
 
 function addon:RestoreDefaults()
     self.update:Hide()
-    
+
     self:IterateChatBubbles("RestoreDefaultsCallback")
 end
 
@@ -205,43 +205,43 @@ local MAX_CHATBUBBLE_WIDTH = 300
 -- Called for each chatbubble, passed the bubble's frame and its fontstring
 function addon:FormatCallback(frame, fontstring)
     -- Optimization: don't process bubbles that aren't shown.
-    if not frame:IsShown() then 
+    if not frame:IsShown() then
         -- Clear last text so that if the same text is spoken again the bubble
         -- will be reprocessed
         fontstring.lastText = nil
-        return 
+        return
     end
-   
+
     -- Handle dynamic expansion/compression of the chat bubble.
     -- Note: The text is not actually changed in this step
-    if self.shorten then 
+    if self.shorten then
         local wrap = fontstring:CanWordWrap() or 0
-       
+
         -- If the mouse is over, then expand the bubble
         if frame:IsMouseOver() then
             fontstring:SetWordWrap(1)
         elseif wrap == 1 then
             fontstring:SetWordWrap(0)
-        end 
-    end 
-    
-    -- Keep our max bubble width up to date with the max width that 
+        end
+    end
+
+    -- Keep our max bubble width up to date with the max width that
     -- we observe the default UI using
     MAX_CHATBUBBLE_WIDTH = math.max(frame:GetWidth(), MAX_CHATBUBBLE_WIDTH)
- 
+
     local text = fontstring:GetText() or ""
- 
-    -- Optimization: Dont process text on a given chat bubble more than once  
+
+    -- Optimization: Dont process text on a given chat bubble more than once
     if text == fontstring.lastText then
-        -- Even if we have processed the bubble text, we may still need 
+        -- Even if we have processed the bubble text, we may still need
         -- to handle dynamic expansion/compression
         if self.shorten then
             fontstring:SetWidth(fontstring:GetWidth())
         end
-        return 
+        return
     end
-        
-    if self.color then 
+
+    if self.color then
         -- Color the bubble border the same as the chat
         frame:SetBackdropBorderColor(fontstring:GetTextColor())
     end
@@ -264,13 +264,13 @@ function addon:FormatCallback(frame, fontstring)
 			if ( ICON_TAG_LIST[term] and ICON_LIST[ICON_TAG_LIST[term]] ) then
 				text = string.gsub(text, tag, ICON_LIST[ICON_TAG_LIST[term]] .. "0|t");
 			end
-		end  
+		end
     end
 
     -- We should end up here exactly once per bubble, resize the fontstring so that it
     -- takes up the correct space horizontally (which also sizes the bubble)
-    fontstring:SetText(text)    
-    fontstring.lastText = text  
+    fontstring:SetText(text)
+    fontstring.lastText = text
     fontstring:SetWidth(math.min(fontstring:GetStringWidth(), MAX_CHATBUBBLE_WIDTH - 14))
 end
 
@@ -282,7 +282,7 @@ function addon:RestoreDefaultsCallback(frame, fontstring)
 end
 
 
--- A pretty generic function to process chat bubbles. 
+-- A pretty generic function to process chat bubbles.
 -- This function is also provided under the MIT license, and is free
 -- for you to reuse in your own addons
 function addon:IterateChatBubbles(funcToCall)
@@ -299,7 +299,7 @@ function addon:IterateChatBubbles(funcToCall)
                     local fontstring = v
                     if type(funcToCall) == "function" then
                         funcToCall(frame, fontstring)
-                    else 
+                    else
                         self[funcToCall](self, frame, fontstring)
                     end
                 end
